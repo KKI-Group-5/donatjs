276aa1044fa96dfc8f199de5ed09691ee6638bbc
Aldebaran64 <aldebaran26adhitya@gmail.com>
2026-04-16 17:57:58 +0700

    Fixed issues and bugs related to previous milestone

--- src/main/java/id/ac/ui/cs/advprog/donatjs/config/SecurityConfig.java
+++ src/main/java/id/ac/ui/cs/advprog/donatjs/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,15 +13,21 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable()) // Disabled for local development convenience
                 .authorizeHttpRequests(auth -> auth
                         // Open to guests
-                        .requestMatchers("/", "/h2-console/**", "/api/campaigns/**", "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+                        .requestMatchers("/", "/h2-console/**", "/api/campaigns/**", "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html", "/api/auth/**").permitAll()
                         // Require the user to be logged in
                         .anyRequest().authenticated()
                 )
+                .formLogin(form -> form.defaultSuccessUrl("/api/profile/me", true))
                 .oauth2Login(oauth2 -> oauth2
                         // If login is successful, send user to this URL
                         .defaultSuccessUrl("/api/profile/me", true)
