1098d3c5ba501b575cbf100389744b7c8611f42b
kingfrog711 <gunata.sakri@gmail.com>
2026-05-20 20:45:23 +0700

    feat(wallet): add REFUND transaction type

--- src/main/java/id/ac/ui/cs/advprog/donatjs/model/TransactionType.java
+++ src/main/java/id/ac/ui/cs/advprog/donatjs/model/TransactionType.java
@@ -1,5 +1,5 @@
 package id.ac.ui.cs.advprog.donatjs.model;
 
 public enum TransactionType {
-    DEPOSIT, WITHDRAWAL, DONATION, SUBSCRIPTION
+    DEPOSIT, WITHDRAWAL, DONATION, SUBSCRIPTION, REFUND
 }
